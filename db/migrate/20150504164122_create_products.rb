6a083c39220552bdcd18962084b7e4707a139abd
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2015-05-10 17:15:32 +0100

    Iteration F3 - F5

--- db/migrate/20150504164122_create_products.rb
+++ db/migrate/20150504164122_create_products.rb
@@ -2,11 +2,47 @@ class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
       t.string :title
+      # also as 
+      # t.column :title, :string
       t.text :description
       t.string :image_url
       t.decimal :price, precision: 8, scale: 2
 
       t.timestamps null: false
+      
+      # same as 
+      # t.datetime :created_at, null: false
+      # t.datetime :updated_at, null: false
     end
   end
 end
+
+# generally:
+
+# create_table :table do |t|
+#   t.column :name, :type, options
+#   t.type :name, options
+# end
+
+# Table column types
+
+# binary
+# boolean
+# date
+# datetime
+# decimal
+# float
+# integer
+# string
+# text
+# time
+
+
+# Table column options
+
+# limit: size
+# default: value
+# null: true/false
+
+# precision: number
+# scale: number
